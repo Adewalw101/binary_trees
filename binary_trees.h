76bb6b801ca60f49070ccc48e93b23236ff855f5
Adewalw101 <elijah2ade@yahoo.co.uk>
2023-05-31 00:18:33 -0700

    Binary and file

--- binary_trees.h
+++ binary_trees.h
@@ -80,4 +80,4 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
-#endif /* __BINARY_T
+#endif /* __BINARY_TREES_H__ */
